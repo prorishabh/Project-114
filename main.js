92b632916fddbbe766c469c30a69d9d4881f6f9a
prorishabh <93649567+prorishabh@users.noreply.github.com>
2023-03-12 09:54:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+
+function take_snapshot()
+{
+    save('Picture_Filter.png');  
+}
+
+function draw()
+{
+    
+}
